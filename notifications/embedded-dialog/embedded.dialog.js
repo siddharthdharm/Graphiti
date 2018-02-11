64829a382aa20c021aa6f6f49b92a6dc4004e772
Siddharth Dharm <siddharthdharm@gmail.com>
2018-02-11 14:25:28 +0530

    Cloned standard design patterns and necessary templates from the offical Microsoft Office Website

---
+++ notifications/embedded-dialog/embedded.dialog.js
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
+// See LICENSE in the project root for license information.
+
+(function () {
+    "use strict";
+
+
+    // The initialize function must be run each time a new page loads.
+    Office.initialize = function () {
+
+        
+        $(document).ready(function () {
+            
+            //Shows the dialog in the task pane when the button is clicked. 
+            document.getElementById("buttonCommand").onclick = function () {                
+                $("#showEmbeddedDialog").css('visibility', 'visible');
+            }
+
+            // Hides the dialog.
+            document.getElementById("buttonClose").onclick = function () {
+                $("#showEmbeddedDialog").css('visibility', 'hidden');
+            }
+
+            // Cancels the action and closes the dialog.
+            document.getElementById("buttonCancel").onclick = function () {
+                $("#showEmbeddedDialog").css('visibility', 'hidden');
+            }
+
+            // Performs the action and closes the dialog.
+            document.getElementById("buttonOk").onclick = function () {
+                // Do action here.
+                $("#showEmbeddedDialog").css('visibility', 'hidden');
+            }
+        });
+
+    };
+
+   
+
+   
+}());
